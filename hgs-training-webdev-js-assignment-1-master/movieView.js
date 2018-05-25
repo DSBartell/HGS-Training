e694f5b3e6086b1aad9c7e2270fdec220f9f2227
Daniye S. Bartell <daniyebartells@gmail.com>
2018-05-24 21:36:07 -0400

    functional changes and .json file

--- hgs-training-webdev-js-assignment-1-master/movieView.js
+++ hgs-training-webdev-js-assignment-1-master/movieView.js
@@ -2,17 +2,51 @@
 $(document).ready(init);
 
   $('#addMovie').click(function()
-  {});
+  {
+    appendMovieDiv();
+  });
 
 /*
 
   Your code goes here
 
 */
-function appendMovieDiv(movie)
+function appendMovieDiv()
 {
-  var movieElement;
-
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'movies.json', true);
+  xhr.onload = function()
+  {
+    if(this.status == 200)
+    {
+      var movies = JSON.parse(this.responseText);
+      console.log(movies);
+      var output ='';
+      for(var i in movies)
+      {
+        output += '<div class="movie-text p-2 m-2 text-center">'+
+        '<p>'+movies[i].title+'</p>'+
+        '<p>'+movies[i].description+'</p>'+
+        '<p>Movie ID: '+movies[i].id+'</p>'+
+        '<button class="btn btn-info link-button"><a href="'+movies[i].webLink+'">Learn More!</a></button>'+
+        '</div>'
+      }
+      document.getElementById('movieView').innerHTML += output;
+    }
+    else if(this.status == 403)
+    {
+      alert('HTTP Request Error 403: Access Forbidden');
+    }
+    else if(this.status == 404)
+    {
+      alert('HTTP Request Error 404: Not Found');
+    }
+    else
+    {
+      alert('HTTP Request Error ' + this.status);
+    }
+  }
+  xhr.send();
   // create a div for the movie
   //$('#movieView').append();
 }
